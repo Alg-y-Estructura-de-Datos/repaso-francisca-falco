c7c7693675d5b6056747418d4f4c866fd7d7ec32
Franciscafalco <franciscafalco4@gmail.com>
2024-08-05 11:52:14 -0300

    Se subio el ejercicio 1

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void intercambiarJugos(float &vaso1,float &vaso2);
+
 int main() {
+    float vaso1,vaso2;
+    cout<<"Ingrese la cantidad de jugo en el vaso 1 (en litros): ";
+    cin>>vaso1;
+    cout<<"Ingrese la cantidad de jugo en el vaso 2 (en litros): ";
+    cin>>vaso2;
 
-    cout << "Examen Final Prog 3" << endl;
+    intercambiarJugos(vaso1,vaso2);
 
-    return 0;
+    cout<<"La cantidad de jugo en el vaso 1 luego del intercambio es: "<<vaso1<<endl;
+    cout<<"La cantidad de jugo en el vaso 2 luego del intercambio es: "<<vaso2<<endl;
+}
+void intercambiarJugos(float &vaso1,float &vaso2){
+    double temp=vaso1;
+    vaso1=vaso2;
+    vaso2=temp;
 }
